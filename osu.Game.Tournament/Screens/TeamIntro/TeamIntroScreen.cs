85b5c1c81ee17c2720dd137186f1a34be1becb9f
methalox <hola@chat.com>
2025-05-11 19:05:28 +0200

    Teamintro terminado creo

--- osu.Game.Tournament/Screens/TeamIntro/TeamIntroScreen.cs
+++ osu.Game.Tournament/Screens/TeamIntro/TeamIntroScreen.cs
@@ -3,8 +3,10 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Extensions.LocalisationExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics;
 using osu.Game.Tournament.Components;
 using osu.Game.Tournament.Models;
 using osuTK;
@@ -30,6 +32,8 @@ namespace osu.Game.Tournament.Screens.TeamIntro
                 mainContainer = new Container
                 {
                     RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.TopCentre,
+                    Origin = Anchor.TopCentre,
                 }
             };
         }
@@ -43,31 +47,52 @@ namespace osu.Game.Tournament.Screens.TeamIntro
             if (match.NewValue == null)
                 return;
 
-            const float y_flag_offset = 292;
-
-            const float y_offset = 460;
+            const float x_centro = 600;
 
             mainContainer.Children = new Drawable[]
             {
                 new RoundDisplay(match.NewValue)
                 {
-                    Position = new Vector2(100, 100)
+                    Position = new Vector2(800, 800),
+                    Colour = Colour4.FromHex("#F24998"),
+                },
+                new TournamentSpriteText
+                {
+                    Position = new Vector2(x_centro, 150),
+                    Text = match.NewValue.Team1.Value!.ToString(),
+                    Font = OsuFont.Inter.With(size: 30, weight: FontWeight.Bold),
+                    Colour = Colour4.FromHex("#A1548C"),
+                },
+                new TournamentSpriteText
+                {
+                    Position = new Vector2(x_centro, 180),
+                    Text = match.NewValue.Team1.Value!.AverageRank.ToLocalisableString() + " [#" + match.NewValue.Team1.Value.Seed + "]",
+                    Font = OsuFont.Inter.With(size: 16, weight: FontWeight.Bold),
+                    Colour = Colour4.FromHex("#A1548C"),
                 },
                 new DrawableTeamFlag(match.NewValue.Team1.Value)
                 {
-                    Position = new Vector2(165, y_flag_offset),
-                },
-                new DrawableTeamWithPlayers(match.NewValue.Team1.Value, TeamColour.Red)
-                {
-                    Position = new Vector2(165, y_offset),
+                    Position = new Vector2(x_centro, 200),
+                    Scale = new Vector2(2f),
                 },
                 new DrawableTeamFlag(match.NewValue.Team2.Value)
                 {
-                    Position = new Vector2(740, y_flag_offset),
+                    Position = new Vector2(x_centro, 420),
+                    Scale = new Vector2(2f),
                 },
-                new DrawableTeamWithPlayers(match.NewValue.Team2.Value, TeamColour.Blue)
+                new TournamentSpriteText
                 {
-                    Position = new Vector2(740, y_offset),
+                    Position = new Vector2(x_centro, 575),
+                    Text = match.NewValue.Team2.Value!.AverageRank.ToLocalisableString() + " [#" + match.NewValue.Team2.Value.Seed + "]",
+                    Font = OsuFont.Inter.With(size: 16, weight: FontWeight.Bold),
+                    Colour = Colour4.FromHex("#3D6C74"),
+                },
+                new TournamentSpriteText
+                {
+                    Position = new Vector2(x_centro, 590),
+                    Text = match.NewValue.Team2.Value!.ToString(),
+                    Font = OsuFont.Inter.With(size: 30, weight: FontWeight.Bold),
+                    Colour = Colour4.FromHex("#3D6C74"),
                 },
             };
         }
